1c5fe4a9f8f2735ebd9092930ace6020d86c170e
Simon Schick <demwizzy@gmail.com>
2014-09-03 01:28:31 +0200

    Removed debug code from main

--- main.lua
+++ main.lua
@@ -1,7 +1,12 @@
 local includePath = debug.getinfo(1).source:gsub("^@", ""):gsub("main%.lua$", "")
 
 table.insert(package.loaders, function(moduleName)
-	return loadfile(includePath .. moduleName:gsub("%.", "\\") .. ".lua")
+	local succ, err = loadfile(includePath .. moduleName:gsub("%.", "\\") .. ".lua")
+	if not succ then
+		error(err, 2)
+	end
+	return succ
 end)
 
 class = require "class"
+
