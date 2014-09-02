ffbf97c5324adef4d22450d76ebaa0a59806097b
Simon Schick <demwizzy@gmail.com>
2014-09-03 00:43:09 +0200

    Renamed meta_extensions/* to remove redudant type naming, also renamed booleanMeta to meta in boolean.lua

---
+++ meta_extensions/boolean.lua
@@ -0,0 +1,15 @@
+local meta = {}
+meta.__index = meta
+
+local function implication(lhs, rhs)
+	return not rhs or lhs
+end
+
+meta.__lt = implication
+meta.__le = implication
+
+meta.__pow = function(lhs, rhs)
+	return (lhs and not rhs) or (not lhs and rhs)
+end
+
+meta.__unm = function(subject) return not subject end
