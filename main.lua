1fce02e0cd3da291bba80b35623052ed6969fa64
Simon Schick <demwizzy@gmail.com>
2014-09-03 19:23:51 +0200

    Added import system which allows to load files like this: local MyClass = import.classes.MyClass

--- main.lua
+++ main.lua
@@ -1,6 +1,7 @@
-local includePath = debug.getinfo(1).source:gsub("^@", ""):gsub("main%.lua$", "")
+includePath = debug.getinfo(1).source:gsub("^@", ""):gsub("main%.lua$", "")
 
 table.insert(package.loaders, function(moduleName)
+	--TODO: allow relative includes
 	local succ, err = loadfile(includePath .. moduleName:gsub("%.", "\\") .. ".lua")
 	if not succ then
 		error(err, 2)
@@ -10,3 +11,25 @@ end)
 
 class = require "class"
 
+local importMeta = {}
+
+local searchPath
+local searchSegments
+
+function importMeta:__index(index)
+	if not searchPath then
+		searchPath = includePath
+		searchSegments = ""
+	end
+	
+	searchPath = searchPath .. (searchPath ~= includePath and "\\" or "") .. index
+	searchSegments = searchSegments .. (searchSegments ~= "" and "." or "") .. index
+	if loadfile(searchPath .. ".lua") then
+		searchPath = includePath
+		searchSegments = ""
+		return require(searchSegments)
+	end
+	return self
+end
+	
+import = setmetatable({}, importMeta)
