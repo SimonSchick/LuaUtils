0782e636676531d3019ce94fb5fae7acb6051408
Simon Schick <demwizzy@gmail.com>
2014-09-06 19:59:31 +0200

    Added syntax checker (WIN32 only so far)

---
+++ syntaxcheck.lua
@@ -0,0 +1,50 @@
+local function identifyOS()
+	return package.config:sub(1, 1) == "\\" and "WIN32" or "UNIX"
+end
+
+local includePath = debug.getinfo(1).source:gsub("^@", ""):gsub("\\syntaxcheck%.lua$", "")
+
+local osName = identifyOS()
+
+if osName == "WIN32" then
+
+	local function dir(params)
+		local proc = io.popen("dir /B " .. params .. " 2>nul")
+		local ret = {}
+		for line in proc:lines() do
+			table.insert(ret, line)
+		end
+		proc:close()
+		return ret
+	end
+
+	local function getFiles(path)
+		return dir("/A-H-D " .. path)
+	end
+	
+	local function getFolders(path)
+		return dir("/A-HD " .. path)
+	end
+	
+	local function findAllFiles(path, extension, ret)
+		ret = ret or {}
+		for _, folder in next, getFolders(path) do
+			findAllFiles(path .. "\\" .. folder, extension, ret)
+		end
+		
+		for _, file in next, getFiles(path) do
+			if file:match("%." .. extension .. "$") then
+				table.insert(ret, path .. "\\" .. file)
+			end
+		end
+		return ret
+	end
+	
+	table.foreach(findAllFiles(includePath, "lua"), function(k, file)
+		local res, err = loadfile(file)
+		if not res then
+			local trimErr = err:gsub(includePath, "")
+			print(trimErr)
+		end
+	end)
+end
