c29c04423afd06da106dd44ad44b1eaefa49d755
Simon Schick <demwizzy@gmail.com>
2014-09-13 07:47:34 +0200

    Added System class which wraps a few os lib functions

---
+++ classes/System.lua
@@ -0,0 +1,18 @@
+local System
+
+System =  class("System", {
+}, {
+	getUserName = function()
+		return System.run("whoami")
+	end,
+	run = function(cmd)
+		local proc = io.popen(cmd)
+		local out = proc:read("*a"):gsub("%s+$", "")
+		proc:close()
+		return out
+	end,
+	getenv = function(var)
+		return os.getenv(var)
+	end
+})
+return System
